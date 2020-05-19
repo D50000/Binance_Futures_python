dc672208066419f33cae8090d00e78fe4b984a3b
D5000 <aoe102198@gmail.com>
2020-05-20 00:43:21 +0800

    add get_mark_price and get_balance function

--- example/market/get_mark_price.py
+++ example/market/get_mark_price.py
@@ -2,10 +2,29 @@ from binance_f import RequestClient
 from binance_f.constant.test import *
 from binance_f.base.printobject import *
 
-request_client = RequestClient(api_key=g_api_key, secret_key=g_secret_key)
+# request_client = RequestClient(api_key=g_api_key, secret_key=g_secret_key)
 
-result = request_client.get_mark_price(symbol="BTCUSDT")
+# result = request_client.get_mark_price(symbol="BTCUSDT")
 
-print("======= Mark Price =======")
-PrintBasic.print_obj(result)
-print("==========================")
+# print("======= Mark Price =======")
+# PrintBasic.print_obj(result)
+# print("==========================")
+
+def _print_obj(obj):
+    price = None
+    if not obj:
+        return -1
+
+    members = [attr for attr in dir(obj) if not callable(attr) and not attr.startswith("__")]
+    for member_def in members:
+        val_str = str(getattr(obj, member_def))
+        if member_def == 'markPrice':
+            print(member_def + ":" + val_str)
+            price = val_str
+            break
+    return price
+
+def get_ETH_price:
+    request_client = RequestClient(api_key=g_api_key, secret_key=g_secret_key)
+    result = request_client.get_mark_price(symbol="ETHUSDT")
+    return float(_print_obj(result))
--- example/trade/get_balance.py
+++ example/trade/get_balance.py
@@ -2,6 +2,7 @@ from binance_f import RequestClient
 from binance_f.constant.test import *
 from binance_f.base.printobject import *
 from binance_f.model.constant import *
+import pandas as pd
 
 """
 {
@@ -17,3 +18,9 @@ def get_balance():
     result = request_client.get_balance()
     # PrintMix.print_data(result)
     return result
+
+def get_balanceAvailableUSDT():
+    request_client = RequestClient(api_key=g_api_key, secret_key=g_secret_key)
+    result = request_client.get_balance()
+    acc_df = pd.DataFrame([acc.__dict__ for acc in result])
+    return acc_df.withdrawAvailable[0]
