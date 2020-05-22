6442b6938eb9d27e04f9bfd66bd78cd8e7dff974
D5000 <aoe102198@gmail.com>
2020-05-23 00:25:34 +0800

    change withdrawAvailable integrate the api account_information

--- example/market/get_mark_price.py
+++ example/market/get_mark_price.py
@@ -10,21 +10,7 @@ from binance_f.base.printobject import *
 # PrintBasic.print_obj(result)
 # print("==========================")
 
-def _print_obj(obj):
-    price = None
-    if not obj:
-        return -1
-
-    members = [attr for attr in dir(obj) if not callable(attr) and not attr.startswith("__")]
-    for member_def in members:
-        val_str = str(getattr(obj, member_def))
-        if member_def == 'markPrice':
-            print(member_def + ":" + val_str)
-            price = val_str
-            break
-    return price
-
 def get_ETH_price():
     request_client = RequestClient(api_key=g_api_key, secret_key=g_secret_key)
     result = request_client.get_mark_price(symbol="ETHUSDT")
-    return float(_print_obj(result))
+    return float(result.markPrice)
